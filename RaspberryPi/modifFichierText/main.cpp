04a84510740b795bac23e07aba7aa61fb5643628
MagueurA <58519238+MagueurA@users.noreply.github.com>
2020-03-06 12:21:17 +0100

    Add files via upload

---
+++ RaspberryPi/modifFichierText/main.cpp
@@ -0,0 +1,45 @@
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    string strReplace = "static ip_address=192.168.1.143/24";
+    string strNew = "static ip_address=192.168.1.145/24";
+    ifstream filein("dhcpcd.conf"); //File to read from
+    ofstream fileout("newdhcpcd.conf"); //Temporary file
+    if(!filein || !fileout)
+    {
+        cout << "Error opening files!" << endl;
+        return 1;
+    }
+
+    string strTemp;
+    //bool found = false;
+    while(getline(filein,strTemp))
+    {
+        if(strTemp == strReplace){
+            strTemp = strNew;
+            //found = true;
+        }
+        strTemp += "\n";
+        fileout << strTemp;
+        //if(found) break;
+    }
+    
+      if( remove( "dhcpcd.conf" ) != 0 )
+    perror( "Error deleting file" );
+    
+  else
+    puts( "File successfully deleted" );
+
+    
+    if (rename("newdhcpcd.conf", "dhcpcd.conf")!=0) 
+        perror( "Error deleting file" );
+  else
+    puts( "File successfully renamed" );
+    
+    return 0;
+}
